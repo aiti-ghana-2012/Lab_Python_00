ef718e3b316c81dbc57636074540a070ba6d9319
Louis Sobel <sobel@lsmbp.local>
2012-06-24 22:16:19 +0000

    Solutions

---
+++ solutions/Day1.py
@@ -0,0 +1,13 @@
+"""
+Python_Lab_00
+Solutions for Exercise 1.3
+"""
+
+
+
+print "Louis Sobel"
+
+
+print "Hello AITI Students!"
+
+
---
+++ solutions/Errors.py
@@ -0,0 +1,11 @@
+"""
+Lab_Python_00
+Solutions for Exercise 1.4
+"""
+
+
+a = 95
+
+#this line will raise a NameError
+#because the variable 'c' is not yet defined
+print c
